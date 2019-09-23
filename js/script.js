8a74fc1de71a641daf466bca6d6be7ab0011ef5a
rockyWebDev <rocky.webdev@gmail.com>
2019-09-23 20:42:02 +0000

    Created the remove button and function

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
 let mainTask= document.getElementById("main-tasks");
 let addBtn= document.getElementById("add");
 function addTask(){
-let task= document.getElementById("task-name").value;
+	let task= document.getElementById("task-name").value;
 	let newTask=document.createElement("p");
+	newTask.setAttribute("class","task");
 	let newLine=document.createTextNode(task);
 	newTask.appendChild(newLine);
-
+	let newButton=document.createElement("button");
+	newButton.setAttribute("onclick","removeTask()")
+	newButton.setAttribute("id","remove")
+	let removeBtn=document.createTextNode("\u2716");
+	newButton.appendChild(removeBtn);
+	newTask.appendChild(newButton);
 	mainTask.append(newTask);
+}
+function removeTask(){
+	let currentTask=document.activeElement.parentNode;
+	currentTask.parentNode.removeChild(currentTask);
 }
